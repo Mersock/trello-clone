c33a3ae7e40a56d84cb74ea70990ca701df7d31d
Phumthawan Nokun <knzphumthawan@gmail.com>
2025-09-19 11:59:58 +0700

    add socket to bring client joining board

--- src/components/board/index.tsx
+++ src/components/board/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box } from '@chakra-ui/react';
 import UserNavbar from '@/src/components/user-navbar';
 import SubNavbar from '@/src/components/sub-navbar';
@@ -7,8 +7,24 @@ import { useAppSelector } from '@/src/hooks';
 import BoardColumns from '@/src/components/board/columns';
 import PropType from 'prop-types';
 
+import { useSocket } from '@/src/context/SocketContext';
+
 const Board = (): JSX.Element => {
   const board = useAppSelector((state) => state.board.board);
+  const { socket } = useSocket();
+
+  useEffect(() => {
+    const slug = board._id;
+    if (socket && slug) {
+      socket.emit('join-board', slug);
+    }
+
+    return () => {
+      if (socket && slug) {
+        socket.emit('leave-board', slug);
+      }
+    };
+  }, [socket, board]);
 
   return (
     <Box
